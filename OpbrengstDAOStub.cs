c125278aae99eaa5c9f5a28a2d01bfc7b589f418
Niels Kelchtermans <kelchtermansniels@gmail.com>
2020-01-17 12:39:59 +0100

    Add project files.

---
+++ OpbrengstDAOStub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDoefening
+{
+    public class OpbrengstDAOStub : IOpbrengstDAO
+    {
+        public decimal TotaleOpbrengst()
+        {
+            return 1000m;
+        }
+    }
+}
---
+++ WinstService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDoefening
+{
+    public class WinstService
+    {
+        private readonly IOpbrengstDAO opbrengstDAO;
+        private readonly IKostDAO kostDAO;
+        public WinstService(IOpbrengstDAO opbrengstDAO, IKostDAO kostDAO)
+        {
+            this.kostDAO = kostDAO;
+            this.opbrengstDAO = opbrengstDAO;
+        }
+        //property readonly
+        public decimal Winst
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+            
+    }
+}

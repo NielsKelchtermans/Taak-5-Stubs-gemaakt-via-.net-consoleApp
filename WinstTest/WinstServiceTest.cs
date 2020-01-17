1012c47df6da25078210cebbbe7b28f5eb5435e7
Niels Kelchtermans <kelchtermansniels@gmail.com>
2020-01-17 16:59:22 +0100

    Moq implementatie

--- WinstTest/WinstServiceTest.cs
+++ WinstTest/WinstServiceTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TDDoefening;
+using Moq;
 
 namespace WinstServiceUnitTest
 {
@@ -12,18 +13,33 @@ namespace WinstServiceUnitTest
         private IOpbrengstDAO opbrengstDAO;
         private WinstService winstService;
 
+        private Mock<IOpbrengstDAO> mockOpbrengstDAO;
+        private Mock<IKostDAO> mockKostDAO;
+
         [TestInitialize]
         public void Initialize()
         {
-            kostDAO = new KostDAOStub();
-            opbrengstDAO = new OpbrengstDAOStub();
+            //kostDAO = new KostDAOStub();
+            //opbrengstDAO = new OpbrengstDAOStub();
+            mockOpbrengstDAO = new Mock<IOpbrengstDAO>();
+            mockKostDAO = new Mock<IKostDAO>();
+            opbrengstDAO = mockOpbrengstDAO.Object;
+            kostDAO = mockKostDAO.Object;
+
+            mockKostDAO.Setup(eenKostDAO => eenKostDAO.TotaleKost()).Returns(169m);
+            mockOpbrengstDAO.Setup(eenOpbrengstDAO => eenOpbrengstDAO.TotaleOpbrengst()).Returns(200m);
+
+
+
             winstService = new WinstService(opbrengstDAO, kostDAO);
         }
 
         [TestMethod]
         public void VindWinst()
         {
-            Assert.AreEqual(900m, winstService.Winst);
+            Assert.AreEqual(31m, winstService.Winst);
+            mockKostDAO.Verify(eenKostDAO => eenKostDAO.TotaleKost());
+            mockOpbrengstDAO.Verify(eenOpbrengstDAO => eenOpbrengstDAO.TotaleOpbrengst());
         }
     }
 }
